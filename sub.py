6499f24507edd77d17eb1f81cd9b8f77a2609887
rakshitaramdurg57-ship-it <rakshitaramdurg57@gmail.com>
2026-01-19 15:10:44 +0530

    Create sub.py

---
+++ sub.py
@@ -0,0 +1,8 @@
+age=int(input("Enter your age:"))
+if age>18:
+    print(f"Enter you are eligible:")
+    Else:
+        print(f"Enter you are not eligible:")
+        Except value Error:
+            print(f"Enter a valid for a age")
+        
